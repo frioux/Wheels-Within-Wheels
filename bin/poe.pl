ca67c0be3384bd02927496b5452d836bc3c350d6
Rocco Caputo <rcaputo@cpan.org>
2013-03-24 10:01:55 -0500

    use meaningful useragent name
    I wouldn't use POE::Component::Client::HTTP's default alias.  It's
    deliberately silly to persuade serious code to change it.  And nobody will
    know what the post() is doing unless there's an antecedent for the alias.

--- bin/poe.pl
+++ bin/poe.pl
@@ -1,14 +1,14 @@
 use POE qw(Component::Client::HTTP);
 use HTTP::Request;
 
-POE::Component::Client::HTTP->spawn;
+POE::Component::Client::HTTP->spawn( Alias => 'useragent' );
 POE::Session->create(
    inline_states => {
       _start => sub { $_[KERNEL]->call($_[SESSION], 'tick') },
       tick => sub {
          $_[KERNEL]->alarm( tick => time() + 60 );
          $_[KERNEL]->post(
-            'weeble', 'request', 'response',
+            'useragent', 'request', 'response',
             HTTP::Request->new(GET => 'http://git.shadowcat.co.uk/gitweb/'),
          );
       },
