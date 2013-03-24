11913e642ffb2b4d18e7038face6b43de67175ae
Rocco Caputo <rcaputo@cpan.org>
2013-03-24 10:09:21 -0500

    factor out request method
    Since "_start" only exists to transfer execution to "tick", one can define
    a named callback and use it for both events.  You don't often see this,
    but POE supports it by design.

--- bin/poe.pl
+++ bin/poe.pl
@@ -4,16 +4,18 @@ use HTTP::Request;
 POE::Component::Client::HTTP->spawn( Alias => 'useragent' );
 POE::Session->create(
    inline_states => {
-      _start => sub { $_[KERNEL]->call($_[SESSION], 'tick') },
-      tick => sub {
-         $_[KERNEL]->delay( tick => time() + 60 );
-         $_[KERNEL]->post(
-            'useragent', 'request', 'response',
-            HTTP::Request->new(GET => 'http://git.shadowcat.co.uk/gitweb/'),
-         );
-      },
+      _start => \&make_request,
+      tick => \&make_request,
       response => sub { warn $_[ARG1][0]->content },
    }
 );
 
 POE::Kernel->run;
+
+sub make_request {
+   POE::Kernel->delay( tick => time() + 60 );
+   POE::Kernel->post(
+      'useragent', 'request', 'response',
+      HTTP::Request->new(GET => 'http://git.shadowcat.co.uk/gitweb/'),
+   );
+}
